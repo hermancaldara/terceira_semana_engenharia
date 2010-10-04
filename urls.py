fd782db58c652cfbab39d004d9c5c422a33393d0
Herman Caldara <hermancaldara@gmail.com>
2010-10-03 22:48:12 -0300

    Added the url for the site media

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,4 +7,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^$', 'terceira_semana_engenharia.views.index'),
+    (r'^site_media/(.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
 )
